913dc98d98d08e809c0dfb91c005d07c42da5a61
自分のアカウント名 <>
2022-09-03 14:51:17 +0900

    judge2

--- judge_maker.py
+++ judge_maker.py
@@ -1,6 +1,6 @@
 def make_judge(grade, points):
-    is_retest = ()
-    is_out = ()
+    is_retest = True if sum((data <= 30 for data in points)) >= 3 else False 
+    is_out = any((data < 10 for data in points))
     if is_out:
         return 3
     elif is_retest:
